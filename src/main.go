ab71480b1a20e045c20bd4e778d6d59277d153c8
sasagawa <sasagawasan@hotmail.co.jp>
2022-03-14 16:05:57 +0900

    ポート指定

--- src/main.go
+++ src/main.go
@@ -10,9 +10,10 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "12121",
+			"message": "1212112345",
 		})
 	})
 	fmt.Println("hello")
-	r.Run()
+	fmt.Println("hello123")
+	r.Run(":8080")
 }
